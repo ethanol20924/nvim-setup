0422e5c3de474b018c40f4d471473a7e2125de36
Ethan Lo <ethan.lo@uqconnect.edu.au>
2023-09-05 23:03:34 +1000

    missed some config

--- lua/custom/plugins/glsl.lua
+++ lua/custom/plugins/glsl.lua
@@ -4,10 +4,14 @@ return {
 		"timtro/glslView-nvim",
 		ft = "glsl",
 		config = function()
-			require("glslView").setup()
+			require("glslView").setup {
+				viewer_path = 'glslViewer',
+				args = { '-l' },
+			}
 			local map = vim.api.nvim_set_keymap
 
-			map("n", "<leader>gv", "<Cmd>GlslView -w 128 -h 256<CR>", { noremap = true, silent = true, desc = "[G]lsl [V]iew" })
+			map("n", "<leader>gv", "<Cmd>GlslView -w 128 -h 256<CR>",
+				{ noremap = true, silent = true, desc = "[G]lsl [V]iew" })
 		end,
 	},
 }
