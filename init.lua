72617c12c3259b8e7de6a490b920829952a17e97
Ethan Lo <ethan.lo@uqconnect.edu.au>
2023-10-26 19:25:49 +1000

    add magit for as git integration

--- init.lua
+++ init.lua
@@ -173,11 +173,11 @@ require("lazy").setup({
 	{
 		-- Add indentation guides even on blank lines
 		"lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup {
-        exclude = { filetypes = {"dashboard"} }
-      }
-    end,
+		config = function()
+			require("ibl").setup({
+				exclude = { filetypes = { "dashboard" } },
+			})
+		end,
 	},
 
 	-- "gc" to comment visual regions/lines
@@ -305,23 +305,12 @@ vim.g.indent_blankline_filetype_exclude = { "help", "vimdoc", "dashboard" }
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set(
-	"n",
-	"k",
-	"v:count == 0 ? 'gk' : 'k'",
-	{ expr = true, silent = true }
-)
-vim.keymap.set(
-	"n",
-	"j",
-	"v:count == 0 ? 'gj' : 'j'",
-	{ expr = true, silent = true }
-)
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group =
-	vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
@@ -341,32 +330,27 @@ require("telescope").setup({
 			},
 		},
 	},
+	extensions = {
+		undo = {
+			use_delta = true,
+			side_by_side = true,
+			layout_strategy = "vertical",
+			layout_config = {
+				preview_height = 0.8,
+			},
+		},
+	},
 })
 
 require("telescope").load_extension("undo")
-vim.keymap.set(
-	"n",
-	"<leader>su",
-	"<cmd>Telescope undo<cr>",
-	{ desc = "[S]earch [U]ndo Tree" }
-)
+vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>", { desc = "[S]earch [U]ndo Tree" })
 
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
 
 -- See `:help telescope.builtin`
-vim.keymap.set(
-	"n",
-	"<leader>?",
-	require("telescope.builtin").oldfiles,
-	{ desc = "[?] Find recently opened files" }
-)
-vim.keymap.set(
-	"n",
-	"<leader><space>",
-	require("telescope.builtin").buffers,
-	{ desc = "[ ] Find existing buffers" }
-)
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set(
 	"n",
 	"<leader>/",
@@ -374,36 +358,11 @@ vim.keymap.set(
 	{ desc = "[/] Fuzzily search in current buffer" }
 )
 
-vim.keymap.set(
-	"n",
-	"<leader>sf",
-	require("telescope.builtin").find_files,
-	{ desc = "[S]earch [F]iles" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>sh",
-	require("telescope.builtin").help_tags,
-	{ desc = "[S]earch [H]elp" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>sw",
-	require("telescope.builtin").grep_string,
-	{ desc = "[S]earch current [W]ord" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>sg",
-	require("telescope.builtin").live_grep,
-	{ desc = "[S]earch by [G]rep" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>sd",
-	require("telescope.builtin").diagnostics,
-	{ desc = "[S]earch [D]iagnostics" }
-)
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -518,16 +477,8 @@ local on_attach = function(_, bufnr)
 	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-	nmap(
-		"<leader>ds",
-		require("telescope.builtin").lsp_document_symbols,
-		"[D]ocument [S]ymbols"
-	)
-	nmap(
-		"<leader>ws",
-		require("telescope.builtin").lsp_dynamic_workspace_symbols,
-		"[W]orkspace [S]ymbols"
-	)
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
 	-- See `:help K` for why this keymap
 	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
@@ -535,16 +486,8 @@ local on_attach = function(_, bufnr)
 
 	-- Lesser used LSP functionality
 	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap(
-		"<leader>wa",
-		vim.lsp.buf.add_workspace_folder,
-		"[W]orkspace [A]dd Folder"
-	)
-	nmap(
-		"<leader>wr",
-		vim.lsp.buf.remove_workspace_folder,
-		"[W]orkspace [R]emove Folder"
-	)
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
 	nmap("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, "[W]orkspace [L]ist Folders")
@@ -569,19 +512,19 @@ local servers = {
 
 	texlab = {},
 
-  pylsp = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = {'W291', 'E261', 'W293'},
-        },
-        ruff = {
+	pylsp = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					ignore = { "W291", "E261", "W293" },
+				},
+				ruff = {
 					enabled = true,
 					extendSelect = { "I" },
 				},
-      }
-    }
-  },
+			},
+		},
+	},
 
 	lua_ls = {
 		Lua = {
---
+++ lua/custom/plugins/magit.lua
@@ -0,0 +1,16 @@
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"nvim-telescope/telescope.nvim", -- optional
+		"sindrets/diffview.nvim", -- optional
+		"ibhagwan/fzf-lua", -- optional
+	},
+	config = function()
+		require("neogit").setup()
+
+		local map = vim.api.nvim_set_keymap
+
+		map("n", "<leader>go", "<Cmd>Neogit<CR>", { noremap = true, silent = true, desc = "[G]it [O]pen" })
+	end,
+}
