3da7b9bd19c748cd377f67201ffd4a27835f1d93
Ethan Lo <ethan.lo@uqconnect.edu.au>
2024-04-26 22:03:47 +0200

    set neogit to nightly for s&gs

--- lua/custom/plugins/magit.lua
+++ lua/custom/plugins/magit.lua
@@ -1,16 +1,18 @@
 return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"nvim-telescope/telescope.nvim", -- optional
-		"sindrets/diffview.nvim", -- optional
-		"ibhagwan/fzf-lua", -- optional
-	},
-	config = function()
-		require("neogit").setup()
+    "NeogitOrg/neogit",
+    branch = 'nightly',
+    dependencies = {
+        "nvim-lua/plenary.nvim", -- required
+        "nvim-telescope/telescope.nvim", -- optional
+        "sindrets/diffview.nvim", -- optional
+        "ibhagwan/fzf-lua" -- optional
+    },
+    config = function()
+        require("neogit").setup()
 
-		local map = vim.api.nvim_set_keymap
+        local map = vim.api.nvim_set_keymap
 
-		map("n", "<leader>go", "<Cmd>Neogit<CR>", { noremap = true, silent = true, desc = "[G]it [O]pen" })
-	end,
+        map("n", "<leader>go", "<Cmd>Neogit<CR>",
+            {noremap = true, silent = true, desc = "[G]it [O]pen"})
+    end
 }
