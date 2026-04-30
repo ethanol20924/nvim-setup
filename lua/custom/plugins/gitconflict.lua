b8c630ce40d2ff40d75ffc77745a56dbd532f967
ethanlo <ethan.lo@intellidesign.com.au>
2026-04-30 14:07:37 +1000

    Add keybinds for searching through git conflicts

--- lua/custom/plugins/gitconflict.lua
+++ lua/custom/plugins/gitconflict.lua
@@ -1,5 +1,50 @@
 return {
-  "akinsho/git-conflict.nvim",
-  version = "*",
-  config = true,
+	"akinsho/git-conflict.nvim",
+	version = "*",
+	config = function()
+		require("git-conflict").setup()
+		-- Custom keybindings
+		vim.keymap.set(
+			"n",
+			"<leader>gco",
+			":GitConflictChooseOurs<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Choose [O]urs" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gct",
+			":GitConflictChooseTheirs<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Choose [T]heirs" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gcb",
+			":GitConflictChooseBoth<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Choose [B]oth" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gcn",
+			":GitConflictChooseNone<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Choose [N]one" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gc[",
+			":GitConflictPrevConflict<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Previous Conflict" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gc]",
+			":GitConflictNextConflict<cr>",
+			{ silent = true, desc = "[G]it [C]onflict Next Conflict" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gcl",
+			":GitConflictListQf<cr>",
+			{ silent = true, desc = "[G]it [C]onflict [L]ist Conflicts" }
+		)
+	end,
 }
